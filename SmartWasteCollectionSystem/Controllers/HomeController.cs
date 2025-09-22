ccc2cc268513d1cb2e4b7197364ef17aa48c2bae
Jomiel <enriquez.jliquigan@gmail.com>
2025-09-22 19:14:05 +0800

    Admin Dashboard

--- SmartWasteCollectionSystem/Controllers/HomeController.cs
+++ SmartWasteCollectionSystem/Controllers/HomeController.cs
@@ -1,26 +1,54 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SmartWasteCollectionSystem.Interface;
 using SmartWasteCollectionSystem.Models;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace SmartWasteCollectionSystem.Controllers
 {
     public class HomeController : Controller
     {
         private readonly SwcsdbContext _context;
+        private readonly IBaseRepository<GarbageCollectionSchedule> _schedule;
         private readonly ILogger<HomeController> _logger;
+        private readonly IBaseRepository<User> _user;
 
-        public HomeController(ILogger<HomeController> logger, SwcsdbContext context)
+        public HomeController(
+            ILogger<HomeController> logger, 
+            SwcsdbContext context,
+            IBaseRepository<GarbageCollectionSchedule> schedule,
+            IBaseRepository<User> user
+        )
         {
             _logger = logger;
             _context = context;
+            _schedule = schedule;
+            _user = user;
         }
 
         [Authorize]
         public IActionResult Index()
         {
-            var users = _context.Users.ToList();
-            return View(users);
+            var userRole = User.FindFirstValue(ClaimTypes.Role);
+            var dashboardData = new DashboardModel();
+
+            if (userRole == "Admin") {
+                
+                dashboardData.GarbageCollectionSchedules = _schedule.GetByConditionAndIncludes(g =>
+                    g.IsActive == true,
+                    "DayOfWeek",
+                    "FrequencyType"
+                ).ToList();
+
+                dashboardData.Users = _user.GetByCondition(u =>
+                    u.CreatedDate.Year == DateTime.Now.Year &&
+                    u.CreatedDate.Month == DateTime.Now.Month
+                ).ToList();
+
+            }
+
+            return View(dashboardData);
         }
 
         public IActionResult Privacy()
---
+++ SmartWasteCollectionSystem/Models/DashboardModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartWasteCollectionSystem.Models
+{
+    public class DashboardModel
+    {
+        public List<User> Users { get; set; }
+        public List<GarbageCollectionSchedule> GarbageCollectionSchedules { get; set; }
+    }
+}
