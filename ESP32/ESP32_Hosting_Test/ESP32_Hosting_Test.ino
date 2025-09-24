74c9ba0bdba85654882cdc4faf3ef04a3377711e
Jomiel <enriquez.jliquigan@gmail.com>
2025-09-24 21:16:31 +0800

    display when connected

--- ESP32/ESP32_Hosting_Test/ESP32_Hosting_Test.ino
+++ ESP32/ESP32_Hosting_Test/ESP32_Hosting_Test.ino
@@ -3,19 +3,26 @@
 
 WebServer server(80);
 String wifiList = "";
+bool isConnected = false;
+String connectedSSID = "";
 
 void handleRoot() {
-  // Build HTML dynamically with SSIDs
   String page = "<html><body>";
   page += "<h2>WiFi Setup</h2>";
-  page += "<form action='/connect' method='POST'>";
-  page += "<label>SSID:</label><select name='ssid'>";
-  page += wifiList;
-  page += "</select><br><br>";
-  page += "<label>Password:</label><input type='password' name='pass'><br><br>";
-  page += "<input type='submit' value='Connect'>";
-  page += "</form></body></html>";
 
+  if (isConnected) {
+    page += "<p style='color:green;'>✅ Connected to " + connectedSSID + "</p>";
+  } else {
+    page += "<form action='/connect' method='POST'>";
+    page += "<label>SSID:</label><select name='ssid'>";
+    page += wifiList;
+    page += "</select><br><br>";
+    page += "<label>Password:</label><input type='password' name='pass'><br><br>";
+    page += "<input type='submit' value='Connect'>";
+    page += "</form>";
+  }
+
+  page += "</body></html>";
   server.send(200, "text/html", page);
 }
 
@@ -23,16 +30,16 @@ void handleConnect() {
   String ssid = server.arg("ssid");
   String pass = server.arg("pass");
 
-  server.send(200, "text/html", "<html><body><h3>Connecting to " + ssid + "...</h3></body></html>");
+  server.send(200, "text/html", "<html><body><h3>Connecting to " + ssid + "...</h3><meta http-equiv='refresh' content='3;url=/' /></body></html>");
 
-  // Try to connect
   WiFi.begin(ssid.c_str(), pass.c_str());
+  connectedSSID = ssid;
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Scan for SSIDs
+  // Scan networks
   int n = WiFi.scanNetworks();
   for (int i = 0; i < n; ++i) {
     wifiList += "<option value='" + WiFi.SSID(i) + "'>" + WiFi.SSID(i) + "</option>";
@@ -40,6 +47,7 @@ void setup() {
 
   // Start AP
   WiFi.softAP("ESP32-Setup", "12345678");
+  Serial.print("AP IP: ");
   Serial.println(WiFi.softAPIP());
 
   server.on("/", handleRoot);
@@ -49,4 +57,10 @@ void setup() {
 
 void loop() {
   server.handleClient();
+
+  // Check connection status
+  if (WiFi.status() == WL_CONNECTED && !isConnected) {
+    isConnected = true;
+    Serial.println("✅ Connected to " + connectedSSID);
+  }
 }
